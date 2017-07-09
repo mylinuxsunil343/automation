4a6941b3f02d2c56809e9062c71719488448f552
mylinuxsunil343 <mylinux.sunil343@gmail.com>
2017-07-09 22:28:10 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,11 +1,1 @@
-lsblk >> text.txt
-source "/home/sunil/Desktop/shell/splunk.txt"
-echo "password is $password"
-echo "License server is '$Licanse_Server'"
-#cd /opt/splunk/bin
-#./splunk edit user admin -password $password -role admin -auth admin:admin
-
-echo -e "\n" >> text.txt
-echo "[splunktcp://9997]" >> text.txt
-echo "connection_host = ip" >> text.txt
-
+echo "Hi Sunil"
