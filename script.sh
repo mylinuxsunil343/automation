f77eadd9a079767529aaf7d358c5a7145ab4b4af
Sunil <mylinux.sunil343@gmail.com>
2019-09-22 13:46:13 -0400

    system check

--- script.sh
+++ script.sh
@@ -1,1 +1,19 @@
 echo "Hi Sunil"
+echo "------------------------------------------------------------"
+echo "The user $USER has logged at - `date`"
+echo "---------------------OS Release-----------------------------"
+cat /etc/os-release
+echo "---------------------System Variable------------------------"
+echo "Bash : $BASH, version $BASH_VERSION"
+echo "Home directory : $HOME"
+echo "Logname : $LOGNAME"
+echo "OS Type : $OSTYPE"
+echo "Present working directory : $PWD"
+echo "Shell using : $SHELL"
+echo "User Name : $USER"
+echo "Host Name : $HOSTNAME"
+echo "----------------------System usage--------------------------"
+df
+echo "------------------------CPU Info----------------------------"
+cat /proc/cpuinfo
+echo "------------------------------------------------------------" 
